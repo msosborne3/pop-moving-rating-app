55e21aa2cb152c8605b8e31ce3f106d715c98ff9
msosborne3 <msosborne@ymail.com>
2016-12-07 19:23:34 -0600

    created tables

--- db/migrate/20161208010145_create_reviews.rb
+++ db/migrate/20161208010145_create_reviews.rb
@@ -1,4 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[5.0]
   def change
+    create_table :reviews do |t|
+      t.integer :rating
+      t.integer :user_id
+      t.integer :movie_id
+    end
   end
 end
